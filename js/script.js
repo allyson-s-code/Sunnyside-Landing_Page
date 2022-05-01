9c9b52918ff235be5063dadccb1ed2d7581f43e6
allyson_s <allysonsmithstudio@gmail.com>
2022-05-01 08:02:57 -0400

    Added toggle true false for aria expanded and e stop propagation for click event

--- js/script.js
+++ js/script.js
@@ -5,12 +5,27 @@ const hamburgerButton = document.querySelector(".toggle-menu");
 
 hamburgerButton.addEventListener("click", function (e) {
   menu.classList.toggle("show");
+  setAriaExpanded();
+  e.stopPropagation();
 });
 
 /* When clicked outside of dropdown menu menu closes */
 
 document.addEventListener("click", function (e) {
-  if (!e.target == menu) {
+  if (e.target != menu && e.target.parentNode != menu) {
     menu.classList.remove("show");
+    setAriaExpanded();
   }
 });
+
+/* Set aria expanded on menu button */
+
+const setAriaExpanded = function () {
+  var x = hamburgerButton.getAttribute("aria-expanded");
+  if (x == "false") {
+    x = "true";
+  } else {
+    x = "false";
+  }
+  hamburgerButton.setAttribute("aria-expanded", x);
+};
